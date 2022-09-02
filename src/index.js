27d290a6363a2354ae9a493a83126c6a60f38a1d
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-09-03 00:08:59 +0200

    change markup teamItem to handlebars template

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ import { checkedClientActivity } from './js/checkedClientActivity';
 const { activityList } = refs;
 
 activityList.addEventListener('click', checkedClientActivity);
+
+const formFile = document.querySelector('.form__file');
+
+formFile.addEventListener('change', e => {
+  const uploadedFile = document.createElement('p');
+  uploadedFile.classList.add('form__file-upload');
+  uploadedFile.textContent = formFile.value;
+  // formFile.before(uploadedFile);
+  formFile.innerHTML = uploadedFile;
+});
--- src/js/markups.js
+++ src/js/markups.js
@@ -1,8 +1,12 @@
 import employees from '../JSON/employees.json';
-import { makeTeamItemMarkup } from './Templates/makeTeamItemMarkup';
+// import { makeTeamItemMarkup } from './Templates/makeTeamItemMarkup';
+
+import sprite from '../images/icon/vk.svg';
+
+import teamItemTpl from '../templates/team-item.hbs';
 
 import refs from './refs';
 const { teamList } = refs;
 
-const markupTeamList = employees.map(makeTeamItemMarkup).join('');
+const markupTeamList = teamItemTpl({ employees });
 teamList.insertAdjacentHTML('beforebegin', markupTeamList);
