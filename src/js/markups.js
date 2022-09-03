1c2ddffbdcf3f65752333e7386d40caf871dcd25
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-09-03 07:20:15 +0200

    finished team-items template.hbs

--- src/js/markups.js
+++ src/js/markups.js
@@ -1,12 +1,6 @@
 import employees from '../JSON/employees.json';
-// import { makeTeamItemMarkup } from './Templates/makeTeamItemMarkup';
-
-import sprite from '../images/icon/vk.svg';
-
-import teamItemTpl from '../templates/team-item.hbs';
-
+import teamItemsTpl from '../templates/team-items.hbs';
 import refs from './refs';
 const { teamList } = refs;
 
-const markupTeamList = teamItemTpl({ employees });
-teamList.insertAdjacentHTML('beforebegin', markupTeamList);
+teamList.insertAdjacentHTML('beforebegin', teamItemsTpl(employees));
