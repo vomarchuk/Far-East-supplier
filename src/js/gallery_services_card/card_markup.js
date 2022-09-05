3a3ae09d82d9a677222f0e218bea9b73d7ba6a8c
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-09-05 16:45:46 +0200

    add new branches swiper

--- src/js/gallery_services_card/card_markup.js
+++ src/js/gallery_services_card/card_markup.js
@@ -12,37 +12,37 @@ export const makeCardMarkup = card => {
   `;
 };
 
-const test = document.querySelector('.swiper-wrapper');
+// const test = document.querySelector('.swiper-wrapper');
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper, { Navigation, Pagination } from 'swiper';
 
-var swiper = new Swiper('.mySwiper', {
-  pagination: {
-    el: '.swiper-pagination',
-    dynamicBullets: true,
-  },
-});
+// var swiper = new Swiper('.mySwiper', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     dynamicBullets: true,
+//   },
+// });
 
-import collection from '../../JSON/finished_works.json';
-const markup = () => {
-  return collection.map(i => {
-    const {
-      preview,
-      alt,
-      description: { title, description, price },
-    } = i;
-    console.log(i.price);
-    return `
-    <div class="swiper-slide">
-      <img class="card__img" src=${preview}  alt=${alt} />
-      <h2 class="card__title secondary__title">${title}</h2>
-      <p class="card__text text">${description}</p>
-      <p class="text card__price">${price}</p>
-    </div>`;
-  });
-};
+// import collection from '../../JSON/finished_works.json';
+// const markup = () => {
+//   return collection.map(i => {
+//     const {
+//       preview,
+//       alt,
+//       description: { title, description, price },
+//     } = i;
+//     console.log(i.price);
+//     return `
+//     <div class="swiper-slide">
+//       <img class="card__img" src=${preview}  alt=${alt} />
+//       <h2 class="card__title secondary__title">${title}</h2>
+//       <p class="card__text text">${description}</p>
+//       <p class="text card__price">${price}</p>
+//     </div>`;
+//   });
+// };
 
-test.innerHTML = markup();
+// test.innerHTML = markup();
 
 // return `
 //   <img class="card__img" src=${preview}  alt=${alt} />
