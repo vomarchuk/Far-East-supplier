c7e2e4172bc5d0afcbda1371c25f4fd2f17bc7b7
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-09-05 20:28:52 +0200

    finished collection cards

--- src/js/markups.js
+++ src/js/markups.js
@@ -1,20 +1,11 @@
 import employees from '../JSON/employees.json';
 import teamItemsTpl from '../templates/team-items.hbs';
-import { makeCardMarkup } from './gallery_services_card/card_markup';
-import { onCollectionBtnClick } from './gallery_services_card/onCollectionBtnClick';
+import { makeCardsMarkup } from './makeCardsMarkup';
 import worksCards from '../JSON/finished_works.json';
 
 import refs from './refs';
-const { teamList, workCard, ourWorksContainer, customRadioCollection } = refs;
+const { teamList, collectionCards } = refs;
 
-customRadioCollection.forEach(item => {
-  if (item.checked) {
-    console.log(item.checked);
-  }
-});
+collectionCards.insertAdjacentHTML('afterbegin', makeCardsMarkup(worksCards));
 
-export const show = i => (workCard.innerHTML = makeCardMarkup(worksCards[i]));
-ourWorksContainer.addEventListener('click', onCollectionBtnClick);
-
-workCard.innerHTML = makeCardMarkup(worksCards[0]);
 teamList.insertAdjacentHTML('afterbegin', teamItemsTpl(employees));
