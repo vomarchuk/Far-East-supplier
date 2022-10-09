b504cce8f745fd147f6650a0f42360ad7b72eb24
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-10-09 12:10:14 +0200

    finifh section delivery

--- src/js/makeCompletedWorkCardsMarkup.js
+++ src/js/makeCompletedWorkCardsMarkup.js
@@ -39,7 +39,7 @@ export const makeCompletedWorkCardsMarkup = collection => {
     <div class="swiper-slide">
       <img class="card__img" src=${preview}  alt=${alt} />
       <h2 class="card__title secondary__title">${title}</h2>
-      <p class="card__text text">${description}</p>
+      <p class="text card__text">${description}</p>
       <p class="text card__price">${price}</p>
     </div>`;
     })
